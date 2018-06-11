f3ca59c75033dc4e72646bd3b785b7544a9718cb
ezjamad <adam.jamka@ericsson.com>
2018-06-11 02:59:47 +0200

    Added fileNotFound page

--- src/com/springmvc/currencyCalculatorApp/controllers/CurrencyController.java
+++ src/com/springmvc/currencyCalculatorApp/controllers/CurrencyController.java
@@ -173,4 +173,9 @@ public class CurrencyController {
     public Currency getCurrency(@PathVariable("id") String name) {
         return currencyService.getCurrency(name);
     }
+
+    @RequestMapping("*")
+    public String pageNotFound() {
+        return "fileNotFound";
+    }
 }
